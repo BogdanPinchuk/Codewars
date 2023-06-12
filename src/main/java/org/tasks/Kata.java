46bf7547c912348e24dd7718c8b5fa761a3a0ae0
Bogdan Pinchuk <bogdan.pinchuk@rubyplay.com>
2023-06-12 03:57:14 +0300

    Added the How good are you really?

--- src/main/java/org/tasks/Kata.java
+++ src/main/java/org/tasks/Kata.java
@@ -10,4 +10,23 @@ public class Kata {
 				.reverse()
 				.toString();
 	}
+
+	public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
+		long sum = yourPoints;
+
+		for (int point : classPoints){
+			sum += point;
+		}
+
+		double averagePoint = (double) sum / (classPoints.length + 1);
+
+		return yourPoints > averagePoint;
+
+		// 1 realization
+		// Arrays.stream(classPoints).average().orElse(0) < yourPoints;
+		// 2 realization
+		// IntStream.of(classPoints).sum()/classPoints.length < yourPoints;
+		// 3 realization
+		// IntStream.of(classPoints).average().getAsDouble() < yourPoints;
+	}
 }
