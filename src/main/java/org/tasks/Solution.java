0710a8cd9c2bb4f99028485b6c80340e0a5a4262
Bogdan Pinchuk <bogdan.pinchuk@rubyplay.com>
2023-06-15 05:41:14 +0300

    Added the Century From Year

--- src/main/java/org/tasks/Solution.java
+++ src/main/java/org/tasks/Solution.java
@@ -1,8 +1,5 @@
 package org.tasks;
 
-import java.time.Year;
-import java.time.temporal.ChronoUnit;
-
 public class Solution {
     public static String[] stringToArray(String s) {
         return s.split(" ");
@@ -13,8 +10,10 @@ public class Solution {
 
     public static int century(int number) {
         // 1 realization
-        return (int) Year.of(0).until(Year.of(number), ChronoUnit.CENTURIES) +
-                (number % 100 > 0 ? 1 : 0);
+//        return (int) Year.of(0).until(Year.of(number), ChronoUnit.CENTURIES) +
+//                (number % 100 > 0 ? 1 : 0);
+
+        return (number / 100) + (number % 100 > 0 ? 1 : 0);
     }
 
 }
