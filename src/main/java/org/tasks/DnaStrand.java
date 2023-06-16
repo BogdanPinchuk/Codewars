564ce09194da9780d9b03efcfa23b6f4d6fb2e25
Bogdan Pinchuk <bogdan.pinchuk@rubyplay.com>
2023-06-16 07:43:45 +0300

    Added the Complementary DNA

--- src/main/java/org/tasks/DnaStrand.java
+++ src/main/java/org/tasks/DnaStrand.java
@@ -1,7 +1,5 @@
 package org.tasks;
 
-import java.util.stream.Collectors;
-
 public class DnaStrand {
     public static String makeComplement(String dna) {
 //        StringBuilder builder = new StringBuilder();
@@ -18,18 +16,30 @@ public class DnaStrand {
 //        return builder.toString();
 
         // 1 realization
-        return dna.chars()
-                .mapToObj(val -> {
-                    switch (val) {
-                        case 'A' -> { return "T"; }
-                        case 'T' -> { return "A"; }
-                        case 'C' -> { return "G"; }
-                        case 'G' -> { return "C"; }
-                    }
-                    return null;
-                })
-                .collect(Collectors.joining(""));
+//        return dna.chars()
+//                .mapToObj(val -> {
+//                    switch (val) {
+//                        case 'A' -> { return "T"; }
+//                        case 'T' -> { return "A"; }
+//                        case 'C' -> { return "G"; }
+//                        case 'G' -> { return "C"; }
+//                    }
+//                    return null;
+//                })
+//                .collect(Collectors.joining(""));
         // 2 realization
+        char[] array = dna.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+            switch (array[i]) {
+                case 'A' -> array[i] = 'T';
+                case 'T' -> array[i] = 'A';
+                case 'C' -> array[i] = 'G';
+                case 'G' -> array[i] = 'C';
+            }
+        }
+        return String.valueOf(array);
+
+        // 3 realization
 //        dna.replace('A', 't')
 //                .replace('T', 'a')
 //                .replace('G', 'c')
