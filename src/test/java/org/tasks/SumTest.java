8668d6ec3fe813151494fb7287e82c099199f764
Bogdan Pinchuk <bogdan.pinchuk@rubyplay.com>
2023-09-12 04:26:49 +0300

    Added the Beginner Series #3 Sum of Numbers

--- src/test/java/org/tasks/SumTest.java
+++ src/test/java/org/tasks/SumTest.java
@@ -13,5 +13,6 @@ class SumTest {
         assertEquals(-1, s.GetSum(0, -1));
         assertEquals(1, s.GetSum(0, 1));
         assertEquals(-3, s.GetSum(-1, -2));
+        assertEquals(0, s.GetSum(-1, 1));
     }
 }
